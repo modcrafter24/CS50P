4ed3cd09fbadfa2d2de5a5504475ea94ceb092fd
modcrafter24 <monojdas.cs@gmail.com>
2024-10-28 23:07:26 +0530

    einstein.py update

--- pset0/einstein.py
+++ pset0/einstein.py
@@ -1,5 +1,5 @@
-## E =mc^2
+## E = mc^2
 m = int(input("Please Enter mass (in Kg): "))
 c = 300000000
 E = m * pow(c, 2)
-print("Equivalent Energy " + str(E) + " Joules")
+print("Equivalent Energy = " + str(E) + " Joules")
