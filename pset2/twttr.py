58b51fc4256cd13bbff3c6bc5ec0736844f4939b
modcrafter24 <monojdas.cs@gmail.com>
2024-10-29 21:24:55 +0530

    pset2 Initial

--- pset2/twttr.py
+++ pset2/twttr.py
@@ -1,1 +1,12 @@
+vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+input_string = input("Input: ")
+output_string = ''
+for char in input_string:
+    if char in vowel:
+        pass
+    else:
+        output_string += char
+
+print(f"Output: {output_string}")
 
