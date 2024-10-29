bc3402e576c2337a23f41ece0f5b1a10e6377455
modcrafter24 <monojdas.cs@gmail.com>
2024-10-29 23:27:27 +0530

    ok

--- pset1/deep.py
+++ pset1/deep.py
@@ -2,7 +2,7 @@ input_string = input("What is the answer to the Great Question of Life, the Univ
 
 input_string = (input_string.strip()).lower()
 
-if input_string == "42" or input_string == "fourty-two" or input_string == "fourty two":
+if input_string == "42" or input_string == "forty-two" or input_string == "forty two":
     print('Yes')
 else:
     print('No')
---
+++ pset3/fuel.py
@@ -0,0 +1,1 @@
+expression = input("Fraction: ")
