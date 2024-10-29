04682886bfbffd599c0624d78830ef557868826f
modcrafter24 <monojdas.cs@gmail.com>
2024-10-29 21:24:30 +0530

    pset2 Initial

---
+++ pset2/plates.py
@@ -0,0 +1,27 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if not(s[0:2].isalpha()):
+        return False
+    if len(s) > 6 or len(s) < 2:
+        return False
+    if not(s.isalnum()):
+        return False
+    num_str =''
+    for c in s:
+        if c.isdigit():
+            num_str += c
+        if len(num_str) > 0:
+            if num_str[0] == '0':
+                return False
+            if not(s[-1].isdigit()):
+                return False
+    return True
+
+main()
