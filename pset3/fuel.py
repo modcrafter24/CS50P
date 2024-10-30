237f6b426994948d7498726da72e711d9245175a
modcrafter24 <monojdas.cs@gmail.com>
2024-10-30 13:10:20 +0530

    Update fuel.py

--- pset3/fuel.py
+++ pset3/fuel.py
@@ -1,1 +1,19 @@
-expression = input("Fraction: ")
+while True:
+    expression = input("Fraction: ")
+    try:
+        x, y = expression.split("/")
+        if x.isdecimal() and y.isdecimal():
+            x, y = int(x), int(y)
+            if x <= y and y != 0:
+                break
+    except ValueError:
+        pass
+
+percentage = round((x/y)*100)
+
+if percentage <= 1:
+    print('E')
+elif percentage >= 99:
+    print('F')
+else:
+    print(f"{percentage}%")
