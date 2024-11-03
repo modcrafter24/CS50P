175d147f47da233c07c1a186bea9bef5a72ac936
modcrafter24 <monojdas.cs@gmail.com>
2024-11-03 10:09:39 +0530

    refactor figlet.py

--- pset4/figlet.py
+++ pset4/figlet.py
@@ -2,17 +2,11 @@ from pyfiglet import Figlet
 from random import choice
 import sys
 
-if len(sys.argv) > 3:
-    sys.exit("Too many arguments")
-elif len(sys.argv) == 2:
-    sys.exit("Not enough arguments")
-
-figlet = Figlet()
-font_list = figlet.getFonts()
-
 if len(sys.argv) == 1:
     font = choice(font_list)
-if len(sys.argv) == 3:
+elif len(sys.argv) == 2:
+    sys.exit("Not enough arguments")
+elif len(sys.argv) == 3:
     if sys.argv[1] == '-f' or sys.argv[1] == '--font':
         if sys.argv[2] in font_list:
             font = sys.argv[2]
@@ -20,11 +14,13 @@ if len(sys.argv) == 3:
             sys.exit("Invalid Usage")
     else:
         sys.exit("Invalid Usage")
+elif len(sys.argv) > 3:
+    sys.exit("Too many arguments")
 
+figlet = Figlet()
+font_list = figlet.getFonts()
 figlet.setFont(font=font)
 
-input_string = input("Input: ")
-
-print(figlet.renderText(input_string))
+print(figlet.renderText(input("Input: ")))
 
 
